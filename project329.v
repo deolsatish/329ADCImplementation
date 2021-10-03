91529e786568cf29212570fd404cb92e1eac703f
DESKTOP-9ETLLVM\nebula <deol.satish.2001.3@gmail.com>
2021-10-03 23:43:44 +1100

    Compiled last bits of code

--- project329.v
+++ project329.v
@@ -1,13 +1,17 @@
 module project329 (CLOCK_50, KEY, SW, LED, ADC_SCLK,
-ADC_CONVST, ADC_SDO, ADC_SDI);
+ADC_CONVST, ADC_SDO, ADC_SDI,pressure_buttons);
 input CLOCK_50;
 input [0:0] KEY;
 input [2:0] SW;
+input [1:0] pressure_buttons;
 output [7:0] LED;
 input ADC_SDO;
 output ADC_SCLK, ADC_CONVST, ADC_SDI;
 wire [11:0] values [7:0];
-assign LED = values [SW] [11:4];
+assign LED[5:0] = values [SW] [9:4];
+assign LED[6] = !pressure_buttons [0];
+assign LED[7] = !pressure_buttons [1];
+
 adc_control ADC (
 .CLOCK (CLOCK_50),
 .RESET (!KEY[0]),
